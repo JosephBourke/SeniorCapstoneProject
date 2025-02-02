680719b0d96003eca5e0571425979627bf10a096
potatoofsadness <161549133+potatoofsadness@users.noreply.github.com>
2025-02-02 17:21:22 -0600

    POSTs for php

--- website/student_home/index.php
+++ website/student_home/index.php
@@ -1,33 +1,47 @@
 <!DOCTYPE html>
 
 <html>
-	<head>
-		<title>Home</title>
-		<style>
-			.side {
-				width: 50%;
-				float: left;
-			}
-			h2, ul {
-				text-align: center;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="side" id="available">
-			<h2>Available Equipment</h2>
-			<ul>
-				<li><a href="new_request/index.php">Camera 1</a></li>
-				<li><a href="new_request/index.php">Camera 2</a></li>
-				<li><a href="new_request/index.php">Camera 3</a></li>
-				<li><a href="new_request/index.php">Camera 4</a></li>				
-			</ul>
-		</div>
-		<div class="side" id="checkedout">
-			<h2>Checked Out Equipment</h2>
-			<ul>
-				<li><a href="view_request_details/index.html">Camera 5</a></li>
-			</ul>
-		</div>
-	</body>
+
+<head>
+	<title>Home</title>
+	<style>
+		.side {
+			width: 50%;
+			float: left;
+		}
+
+		h2,
+		ul {
+			text-align: center;
+		}
+	</style>
+</head>
+
+<body>
+	<?php
+	$pickupDate = $_POST['pickupDate'];
+	$returnDate = $_POSt['returnDate'];
+	$user = $_POST['user'];
+	$equipment = $_POST['equipment'];
+	?>
+	<div class="side" id="available">
+		<h2>Available Equipment</h2>
+		<ul>
+			<li><a href="new_request/index.php">Camera 1</a></li>
+			<li><a href="new_request/index.php">Camera 2</a></li>
+			<li><a href="new_request/index.php">Camera 3</a></li>
+			<li><a href="new_request/index.php">Camera 4</a></li>
+		</ul>
+	</div>
+	<div class="side" id="checkedout">
+		<h2>Checked Out Equipment</h2>
+		<ul>
+			<li><a href="view_request_details/index.html">Camera 5</a></li>
+			<?php
+			
+			?>
+		</ul>
+	</div>
+</body>
+
 </html>
