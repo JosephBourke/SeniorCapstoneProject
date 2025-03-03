1acc881f9f69579b34d4c00a74c9a1042ecd412a
potatoofsadness <emsullygirl@gmail.com>
2025-03-03 13:26:52 -0600

    fixed testdata.sql
    I fixed the syntax so the code works n stuff.

--- database/testdata.sql
+++ database/testdata.sql
@@ -1,10 +1,30 @@
-INSERT INTO `mydb`.`user` (`username`, `email`, `password`, `create_time`, `uid`, `isfaculty`) VALUES (`steve`, `jone1456@ravens.benedictine.edu`, `MONmon19$`, `2025-03-24`, 1234567, false);
-INSERT INTO `mydb`.`equipment` (`id`, `barcode`, `name`, `description`) VALUES (5461723, 123456789, `Big Camera`, `This camera is really big.`);
-INSERT INTO `mydb`.`user` (`username`, `email`, `password`, `create_time`, `uid`, `isfaculty`) VALUES (`alyssa`, `alex3142@ravens.benedictine.edu`, `GIGtime21%`, `2025-04-18`, 5261627, false);
-INSERT INTO `mydb`.`equipment` (`id`, `barcode`, `name`, `description`) VALUES (1234567, 782345267, `Small Camera`, `This camera is really small.`);
-INSERT INTO `mydb`.`user` (`username`, `email`, `password`, `create_time`, `uid`, `isfaculty`) VALUES (`alexis`, `morg5261@ravens.benedictine.edu`, `TALtal31&`, `2025-04-25`, 4567283, true);
-INSERT INTO `mydb`.`equipment` (`id`, `barcode`, `name`, `description`) VALUES (2567356, 829151727, `VIP Camera`, `This camera is really special.`);
-INSERT INTO `mydb`.`user` (`username`, `email`, `password`, `create_time`, `uid`, `isfaculty`) VALUES (`alex`, `safe1627@ravens.benedictine.edu`, `BIGsmall22#`, `2025-05-01`, 2563764, false);
-INSERT INTO `mydb`.`equipment` (`id`, `barcode`, `name`, `description`) VALUES (2563721, 354866545, `Average Camera`, `This camera is really average.`);
-INSERT INTO `mydb`.`user` (`username`, `email`, `password`, `create_time`, `uid`, `isfaculty`) VALUES (`donald`, `jack2839@ravens.benedictine.edu`, `COMteach60$`, `2025-05-11`, 9876543, true);
-INSERT INTO `mydb`.`equipment` (`id`, `barcode`, `name`, `description`) VALUES (2637827, 345679284, `Complex Camera`, `This camera is really complex.`);
+INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty) 
+VALUES ('steve', 'jone1456@ravens.benedictine.edu', 'MONmon19$', '2025-03-24', 1234567, false);
+
+INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty)  
+VALUES ('alyssa', 'alex3142@ravens.benedictine.edu', 'GIGtime21%', 2025-04-18, 5261627, false);
+
+INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty) 
+VALUES ('alexis', 'morg5261@ravens.benedictine.edu', 'TALtal31&', '2025-04-25', 4567283, true);
+
+INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty) 
+VALUES ('alex', 'safe1627@ravens.benedictine.edu', 'BIGsmall22#', '2025-05-01', 2563764, false);
+
+INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty)  
+VALUES ('donald', 'jack2839@ravens.benedictine.edu', 'COMteach60$', '2025-05-11', 9876543, true);
+
+
+INSERT INTO `mydb`.`equipment` (id, barcode, name, description) 
+VALUES (5461723, 123456789, 'Big Camera', 'This camera is really big.');
+
+INSERT INTO `mydb`.`equipment` (id, barcode, name, description)  
+VALUES (1234567, 782345267, 'Small Camera', 'This camera is really small.');
+
+INSERT INTO `mydb`.`equipment` (id, barcode, name, description)  
+VALUES (2567356, 829151727, 'VIP Camera', 'This camera is really special.');
+
+INSERT INTO `mydb`.`equipment` (id, barcode, name, description) 
+VALUES (2563721, 354866545, 'Average Camera', 'This camera is really average.');
+
+INSERT INTO `mydb`.`equipment` (id, barcode, name, description) 
+VALUES (2637827, 345679284, 'Complex Camera', 'This camera is really complex.');
