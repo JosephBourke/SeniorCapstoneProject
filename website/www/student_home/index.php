db862a6600942752cb44e7135f63e29079f47900
potatoofsadness <emsullygirl@gmail.com>
2025-03-17 14:04:32 -0500

    cameras available - student_home
    Added php so actually shows cameras available from the database. Someone else has some other stuff in there, I didn't really touch that don't worry.

--- website/www/student_home/index.php
+++ website/www/student_home/index.php
@@ -14,24 +14,22 @@
 			text-align: center;
 		}
 
-		table,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
+		table, td {
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
 
 		table.center {
-            margin-left: auto;
-            margin-right: auto;
-        }
+			margin-left: auto;
+			margin-right: auto;
+		}
 
 		body {
-            background:rgb(244, 243, 243);
-            font-family: "Roboto", sans-serif;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-        }
-    
+			background: rgb(244, 243, 243);
+			font-family: "Roboto", sans-serif;
+			-webkit-font-smoothing: antialiased;
+			-moz-osx-font-smoothing: grayscale;
+		}
 	</style>
 </head>
 
@@ -41,32 +39,52 @@
 	$returnDate = $_POSt['returnDate'];
 	$user = $_POST['user'];
 	$equipment = $_POST['equipment'];
+
+	$host = "127.0.0.1";
+	$port = 3306;
+	$socket = "";
+	$user = "faculty";
+	$password = "P@ssw0rd";
+	$dbname = "mydb";
 	?>
 	<div class="side" id="available">
 		<h2>Available Equipment</h2>
-		<table style="width:80%" class="center">
-			<tr><td><a href="new_request/index.php">Camera 1</a></td></tr>
-			<tr><td><a href="new_request/index.php">Camera 2</a></td></tr>
-			<tr><td><a href="new_request/index.php">Camera 3</a></td></tr>
-			<tr><td><a href="new_request/index.php">Camera 4</a></td></tr>
+		<table style="width:80%" class="center"><tr>
+		<?php
+		$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+			or die('Could not connect to the database server' . mysqli_connect_error());
+		
+		$query = "SELECT e.id, e.name from equipment e, checkout c where e.id = c.equipment_id and c.checkintime is null";
+		if ($stmt = $con->prepare($query)) {
+			$stmt->execute();
+			$stmt->bind_result($id, $name);
+			while ($stmt->fetch()) {
+				echo "<tr><td><a href=". "new_request/index.php" .">" . $name . "</option>";
+			}
+			$stmt->close();
+		}
+		?>
 		</table>
 	</div>
 	<div class="side" id="checkedout">
 		<h2>Checked Out Equipment</h2>
 		<?php
 		//if (isset($equipment)) {
-			?>
-			<table>
-				<tr><th>Equipment</th><td><a href="view_request_details/index.html"><?php echo $equipment; ?></a></td></tr>
-				<tr><th>Status</th><td>Pending</td></tr>
-				<tr></tr>
-			</table>
-			<?php
+		?>
+		<table>
+			<tr>
+				<th>Equipment</th>
+				<td><a href="view_request_details/index.html"><?php echo $equipment; ?></a></td>
+				<th>Status</th>
+			</tr>
+			<tr>
+				<td>Pending</td>
+			</tr>
+			<tr></tr>
+		</table>
+		<?php
 		//}
-
-
 		
-
 		?>
 	</div>
 </body>
@@ -74,17 +92,19 @@
 
 
 <?php
+
+
 $equipment_id = $_POST["equipment"];
 $user_id = 1234567;
 $date = date('YmdHis');
 $description = $_POST["returnDate"];
 
-$host = "127.0.0.1";
-$port = 3306;
-$socket = "";
-$user = "faculty";
-$password = "P@ssw0rd";
-$dbname = "mydb";
+// $host = "127.0.0.1";
+// $port = 3306;
+// $socket = "";
+// $user = "faculty";
+// $password = "P@ssw0rd";
+// $dbname = "mydb";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket) or die('Could not connect to the database server' . mysqli_connect_error());
 
