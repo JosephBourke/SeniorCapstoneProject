290399a51db40ee20ea16ac880ac2b6aeccadca1
JosephBourke <bour3754@ravens.benedictine.edu>
2025-03-19 21:37:21 -0500

    DB Setup Simplified
    This consolidates the SQL Setup. Now all of the test data and table generation are together.

--- database/newest.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW `newest` AS
-select lastreturned, lastcheckedout
-from equipment
-order by coalesce(lastreturned, lastcheckedout) desc;
-
-select * from `newest`;
--- database/requests.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW `requests` AS
-select id, create_time, description, accepted, user_id, equipment_id
-from request
-order by id;
-
-select * from `requests`;
