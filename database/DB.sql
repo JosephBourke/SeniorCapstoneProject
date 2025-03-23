c47306d746683d4ec9f1afafdb22d23263a2da35
William Brissett <william.brissett@gmail.com>
2025-03-23 18:02:13 -0500

    Update DB.sql
    Updated file to fix minor mistakes and made delete statements in case anything has to be deleted.

--- database/DB.sql
+++ database/DB.sql
@@ -92,60 +92,64 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-
-
-INSERT INTO user (username, password, uid) VALUES
-('wert1728', 'MONmon19$', 2536471),
-('gert1924', 'GIGtime21%', 4673920),
-('ghet1928', 'TALtal31&', 5261822),
-('newt1923', 'BIGsmall22#', 5362891),
-('seng1927', 'COMteach60$', 9876534);
-
 INSERT INTO request (id, create_time, description, accepted, user_id, equipment_id) 
 VALUES 
-(1234567, '2025-02-18 10:30:00', 'Request for specific Sony camera.', 1, 2536471, 12),
+(1234567, '2025-02-18 10:30:00', 'Request for specific Sony camera.', 1, 1234567, 12),
 (4567890, '2025-02-18 11:00:00', 'Request for Super-Special camera.', 0, 4673920, 15),
 (5673839, '2025-02-18 11:30:00', 'Request for less special camera.', 1, 5261822, 9),
 (2839178, '2025-02-18 12:00:00', 'Request for insanely expensive camera.', 0, 5362891, 18),
 (9876544, '2025-02-18 12:30:00', 'Request for super cheap camera.', 1, 9876534, 20);
 
-INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty) 
-VALUES ('steve', 'jone1456@ravens.benedictine.edu', 'MONmon19$', '2025-03-24', 1234567, false);
+INSERT INTO `user` (username, email, password, create_time, uid, isfaculty) 
+VALUES ('steve', 'jone1456@ravens.benedictine.edu', 'MONmon19$', '2024-03-24', 1234567, false);
 
-INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty)  
-VALUES ('alyssa', 'alex3142@ravens.benedictine.edu', 'GIGtime21%', '2025-04-18', 5261627, false);
+INSERT INTO `user` (username, email, password, create_time, uid, isfaculty)  
+VALUES ('alyssa', 'alex3142@ravens.benedictine.edu', 'GIGtime21%', '2024-04-18', 4673920, false);
 
-INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty) 
-VALUES ('alexis', 'morg5261@ravens.benedictine.edu', 'TALtal31&', '2025-04-25', 4567283, true);
+INSERT INTO `user` (username, email, password, create_time, uid, isfaculty) 
+VALUES ('alexis', 'morg5261@ravens.benedictine.edu', 'TALtal31&', '2024-04-25', 5261822, true);
 
-INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty) 
-VALUES ('alex', 'safe1627@ravens.benedictine.edu', 'BIGsmall22#', '2025-05-01', 2563764, false);
+INSERT INTO `user` (username, email, password, create_time, uid, isfaculty) 
+VALUES ('alex', 'safe1627@ravens.benedictine.edu', 'BIGsmall22#', '2024-05-01', 5362891, false);
 
-INSERT INTO `mydb`.`user` (username, email, password, create_time, uid, isfaculty)  
-VALUES ('donald', 'jack2839@ravens.benedictine.edu', 'COMteach60$', '2025-05-11', 9876543, true);
+INSERT INTO `user` (username, email, password, create_time, uid, isfaculty)  
+VALUES ('donald', 'jack2839@ravens.benedictine.edu', 'COMteach60$', '2024-05-11', 9876534, true);
 
-INSERT INTO `mydb`.`equipment` (id, barcode, name, description) 
+INSERT INTO equipment (id, barcode, name, description) 
 VALUES (12, 123456789, 'Big Camera', 'This camera is really big.');
 
-INSERT INTO `mydb`.`equipment` (id, barcode, name, description)  
+INSERT INTO equipment (id, barcode, name, description)  
 VALUES (15, 782345267, 'Small Camera', 'This camera is really small.');
 
-INSERT INTO `mydb`.`equipment` (id, barcode, name, description)  
+INSERT INTO equipment (id, barcode, name, description)  
 VALUES (9, 829151727, 'VIP Camera', 'This camera is really special.');
 
-INSERT INTO `mydb`.`equipment` (id, barcode, name, description) 
+INSERT INTO equipment (id, barcode, name, description) 
 VALUES (18, 354866545, 'Average Camera', 'This camera is really average.');
 
-INSERT INTO `mydb`.`equipment` (id, barcode, name, description) 
+INSERT INTO equipment (id, barcode, name, description) 
 VALUES (20, 345679284, 'Complex Camera', 'This camera is really complex.');
+
+select * from `user`;
+
+delete from `user`;
+
+select * from equipment;
+
+delete from equipment;
+
 select * from request;
-INSERT INTO checkout
-(id, studentid, equipmentid, checkoutdate, duedate)
+
+delete from request;
+
+INSERT INTO checkout (id, user_id, equipment_id, checkouttime, checkintime, duedate)
 VALUES
-(1234567, 2536471, 12, '2025-02-18 10:30:00', '2025-03-01 10:30:00'),
-(4567890, 4673920, 15, '2025-02-18 11:00:00', '2025-03-01 11:00:00'),
-(5673839, 5261822, 9, '2025-02-18 11:30:00', '2025-03-01 11:30:00'),
-(2839178, 5362891, 18, '2025-02-18 12:00:00', '2025-03-01 12:00:00'),
-(9876544, 9876534, 20, '2025-02-18 12:30:00', '2025-03-01 12:30:00');
+(1234567, 1234567, 12, '2025-02-18 10:30:00', '2025-02-25 10:30:00','2025-03-01 10:30:00'),
+(4567890, 4673920, 15, '2025-02-18 11:00:00', '2025-02-25 10:30:00','2025-03-01 11:00:00'),
+(5673839, 5261822, 9, '2025-02-18 11:30:00', '2025-02-25 10:30:00','2025-03-01 11:30:00'),
+(2839178, 5362891, 18, '2025-02-18 12:00:00', '2025-02-25 10:30:00','2025-03-01 12:00:00'),
+(9876544, 9876534, 20, '2025-02-18 12:30:00', '2025-02-25 10:30:00','2025-03-01 12:30:00');
 
 select * from checkout;
+
+delete from checkout;
