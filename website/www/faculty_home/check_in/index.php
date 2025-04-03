bc2f2e95b04fd10e8232841b094c7cfe32c01c56
Justin Stuesse <justinstuesse@gmail.com>
2025-04-02 21:06:00 -0500

    Mostly done with check in page
    The update statement for updating the check in date is currently not working still.

--- website/www/faculty_home/check_in/index.php
+++ website/www/faculty_home/check_in/index.php
@@ -1,100 +1,72 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
 
-<head>
-  <title>Check In</title>
-  <style>
-    .side {
-      width: 50%;
-      float: left;
-    }
-
-    h2,
-    ul {
-      text-align: center;
-    }
-
-    table,
-    td {
-      border: 1px solid black;
-      border-collapse: collapse;
-    }
-
-    table.center {
-      margin-left: auto;
-      margin-right: auto;
-    }
-
-    body {
-      background: rgb(244, 243, 243);
-      font-family: "Roboto", sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-    }
-
-    header {
-      background-color: rgb(148, 13, 13);
-      color: #fff;
-      padding: 30px;
-      text-align: center;
-      font-size: 30px;
-    }
-
-    section {
-      padding: 20px;
-    }
-
-  </style>
-</head>
-
-<center>
+  <head>
+    <title>Check In</title>
+  </head>
 
   <body>
-    <header>
-      <strong>Check In</strong>
-    </header>
 
     <div class="search-wrapper">
-      <form>
+      <h2>Check In</h2>
 
-        <!-- <h2>Check In</h2> -->
-        <section>
-
-          <table style="width:80%" class="center">
-            <tr>
-              <?php
-              $host = "127.0.0.1";
-              $port = 3306;
-              $socket = "";
-              $user = "faculty";
-              $password = "P@ssw0rd";
-              $dbname = "mydb";
-              // this is currently actually equipment that is not available
-              $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-                or die('Could not connect to the database server' . mysqli_connect_error());
-
-              $query = "SELECT e.id, e.name from equipment e, checkout c where e.id = c.equipment_id and c.checkintime is null";
-              if ($stmt = $con->prepare($query)) {
-                $stmt->execute();
-                $stmt->bind_result($id, $name);
-                while ($stmt->fetch()) {
-                  echo "<tr><td><a>" . $name . "</option>";
-                  ?>
-                  <td><button class="close-icon" type="reset">Check-In</button><?php
-                }
-                $stmt->close();
-              }
-              ?>
-          </table>
-        </section>
-
-
-        <!-- <input type="text" name="focus" required class="search-box" placeholder="Enter the Equipment ID" /> -->
-        <!-- <button class="close-icon" type="reset">Check-In</button> -->
+      <form name="search" id="search" action="index.php" method="post">
+        Student email: <input type="text" name="email" id="email">
+        <input type="submit" value="Search">
       </form>
+
+      <table style="width:80%" class="center">
+          <?php
+          $email = $_POST["email"];
+          $hidden = $_POST["hidden"];
+
+          $host = "192.168.56.101"; // change this to match the ip address for your virtual machine
+          $port = 3306;
+          $socket = "";
+          $user = "faculty";
+          $password = "P@ssw0rd";
+          $dbname = "mydb";
+
+          // this is currently actually equipment that is not available
+          $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+            or die('Could not connect to the database server' . mysqli_connect_error());
+
+          if (isset($hidden)) {
+            // UPDATE statement not working 
+            $now = date("y-m-d H:i:s");
+            $query = "UPDATE checkout SET checkindate = '$now' WHERE id = $hidden";
+            $stmt = $con->prepare($query);
+            $stmt->execute();
+          }
+
+          $query = "SELECT u.uid, u.username, c.equipmentid, c.checkoutdate, c.duedate, c.id, e.name FROM user u, checkout c, equipment e WHERE u.email = '$email' AND u.uid = c.studentid AND c.equipmentid = e.id AND c.checkindate IS NULL";
+          if ($stmt = $con->prepare($query)) {
+            $stmt->execute();
+            $stmt->bind_result($uid, $username, $equipmentid, $checkoutdate, $duedate, $cid, $name);
+            $iter = 0;
+            while ($stmt->fetch()) {
+              if ($iter === 0) {
+                echo "<tr><th>$username</th><th>$uid</th></tr>";
+              }
+              echo <<<END
+              <tr><td>$name</td><td>Checked out: $checkoutdate</td><td>Due: $duedate</td><td>
+              <form name="checkin" id="checkin" action="index.php" method="post">
+                <input type="submit" value="Check in">
+                <input type="hidden" value="<?php echo $cid;?>" name="hidden" id="hidden">
+              </form></td></tr>
+              END;
+              $iter++;
+            }
+
+            
+            $stmt->close();
+          }
+          ?>
+      </table>
+
     </div>
+
   </body>
-</center>
 
 </html>
