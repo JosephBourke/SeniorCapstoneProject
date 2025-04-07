4e6730a56b11b503e62aaa23fb2d01f77361fdfe
Justin Stuesse <justinstuesse@gmail.com>
2025-04-07 10:21:45 -0500

    Refactored the entire codebase
    There are now separate folders for the css, scripts, faculty_home, student_home, and images. More will be explained in the meeting later today.

---
+++ website/www/faculty_home/check_in.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  	<title>Check In</title>
+	<link href="../css/stylesheet.css" type="text/css" rel="stylesheet" />
+
+</head>
+
+<body>
+  	<header>
+    	<strong>Check In</strong>
+  	</header>
+
+	<div class="searchDiv">
+
+		<form name="search" id="search" action="check_in.php" method="post">
+		<strong>Student email: </strong><input type="text" name="email" id="email">
+		<input type="submit" value="Search">
+		</form>
+	</div>
+
+	<div class="search-wrapper">
+		<table style="width:80%" class="center">
+		<?php
+
+
+		$host = "127.0.0.1"; # change this to match the ip address for your virtual machine
+		$port = 3306;
+		$user = "faculty";
+		$password = "P@ssw0rd";
+		$dbname = "MariaSQL";
+
+		# this is currently actually equipment that is not available
+		$con = new mysqli($host, $user, $password, $dbname, $port)
+			or die('Could not connect to the database server' . mysqli_connect_error());
+
+
+		# If the email part is set, it will collect a bunch of information related to that email.
+		if (isset($_POST["email"])) {
+
+			$email = $_POST["email"];
+			$query = <<<MySQL_Query
+						SELECT u.uid, u.username, c.equipmentid, c.checkoutdate, c.duedate, c.id, e.name 
+						FROM user u, checkout c, equipment e 
+						WHERE u.email = '$email' AND u.uid = c.studentid AND c.equipmentid = e.id AND c.checkindate IS NULL
+						MySQL_Query;
+			if ($stmt = $con->prepare($query)) {
+			$stmt->execute();
+			$stmt->bind_result($uid, $username, $equipmentid, $checkoutdate, $duedate, $cid, $name);
+			$iter = 0;
+			while ($stmt->fetch()) {
+
+				# If the iter variable is 0, your username and user ID will print out.
+            	# Afterwards, it willl print out name, checkout date, and due date along with a submit button and a hidden cid value.
+				if ($iter === 0) {
+				echo "<tr><th>$username</th><th>$uid</th></tr>";
+				}
+				echo <<<STR
+					<tr><td>$name</td><td>Checked out: $checkoutdate</td><td>Due: $duedate</td><td>
+					<form name="checkin" id="checkin" action="check_in.php" method="post">
+					<input type="submit" value="Check in">
+					<input type="hidden" value="$cid" name="hidden" id="hidden">
+					</form></td></tr>
+					STR;
+				$iter++;
+			}
+
+
+			}
+			$stmt->close();
+		}
+
+
+
+
+		# If hidden is set, the checkin date on the checkout table will be updated to what is now where id is equal to the hidden variable.
+		if (isset($_POST["hidden"])) {
+			$hidden = $_POST["hidden"];
+			# UPDATE statement not working 
+			$now = date("y-m-d H:i:s");
+			$query = "UPDATE checkout SET checkindate = '$now' WHERE id = $hidden";
+			$stmt = $con->prepare($query);
+			$stmt->execute();
+
+			echo "<script>alert(\"Submission was successful.\");</script>";
+			$stmt->close();
+		}
+		?>
+		</table>
+
+	</div>
+
+</body>
+
+</html>
---
+++ website/www/faculty_home/equipment_view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+
+
+	<div class="search-wrapper">
+		<form>
+			<input type="text" name="focus" required class="search-box" placeholder="Search for Equipment" />
+			<button class="close-icon" type="reset">Check-In</button>
+		</form>
+
+		<ul>
+			<li>Camera A</li>
+			<li>Camera B</li>
+			<li>Camera C</li>
+			<li>Camera D</li>
+			<li>Camera E</li>
+			<li>Camera F</li>
+			<li>Camera G</li>
+			<li>Camera H</li>
+			<li>Camera I</li>
+			<li>Camera J</li>
+			<li>Camera K</li>
+			<li>Camera L</li>
+			<li>Camera M</li>
+			<li>Camera N</li>
+			<li>Camera O</li>
+			<li>Camera P</li>
+			<li>Camera Q</li>
+			<li>Camera R</li>
+			<li>Camera S</li>
+			<li>Camera T</li>
+
+		</ul>
+	</div>
+
+
+
+
+
+</html>
