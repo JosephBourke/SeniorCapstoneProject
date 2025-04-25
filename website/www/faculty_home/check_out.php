725a9f48650d8e7990db5584df0d2cd86cede41b
Justin Stuesse <justinstuesse@gmail.com>
2025-04-24 22:22:57 -0500

    Updated check out
    Check out via barcode, due date, and student name.

--- website/www/faculty_home/check_out.php
+++ website/www/faculty_home/check_out.php
@@ -6,10 +6,7 @@
         <?php
         # These are variables that are filled with the associating data input into the text inputs below
         $studentname = $_POST["studentname"];
-        $studentemail = $_POST["studentemail"];
-        $studentID = $_POST["studentID"];
-        $equipmentname = $_POST["equipmentname"];
-        $equipmentID = $_POST["equipmentID"];
+        $barcode = $_POST["barcode"];
         $duedate = $_POST["duedate"];
         ?>
     </head>
@@ -17,10 +14,7 @@
         <!-- These are the text inputs that are put in the variables above when submit is pressed -->
         <form action="check_out.php" method="post">
             Student Name:   <input type="text" name="studentname"><br>
-            Student Email:  <input type="text" name="studentemail"><br>
-            Student ID:     <input type="number" name="studentID"><br>
-            Equipment Name: <input type="text" name="equipmentname"><br>
-            Equipment ID:   <input type="number" name="equipmentID"><br>
+            Barcode:        <input type="text" name="barcode"><br>
             Due Date:       <input type="date" name="duedate"><br>
             <input type="submit">
         </form>
@@ -39,9 +33,9 @@
         # With this code, if Student ID, Equipment ID and Due Date is set, it changes the date format to a typical specific calander date and time format.
         # After that is done, the checkout date, due date, equipment ID, and user ID variables are inserted into their respective spots on the SQL checkout table.
         # And then, it will produce an alert that says "Submission is successful".
-        if (isset($studentID) && isset($equipmentID) && isset($duedate)) {
+        if (isset($studentname) && isset($barcode) && isset($duedate)) {
             $date = date("y-m-d H:i:s");
-            $result = $conn->query("INSERT INTO checkout (checkoutdate, duedate, equipmentid, studentid) VALUES ('$date', '$duedate " . date("H:i:s") . "', $equipmentID, $studentID)");
+            $result = $conn->query("INSERT INTO checkout (checkoutdate, duedate, barcode, studentname) VALUES ('$date', '$duedate " . date("H:i:s") . "', $barcode, $studentname)");
             if ($result) {
                 echo "<script>alert(\"Submission was successful.\");</script>";
             }
